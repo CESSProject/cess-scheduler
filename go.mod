fcbe5571573cd0418083cf3b2fdd7971cdc02ca8
wolf-dep <wolf@gmail.com>
2022-03-03 21:38:30 -0800

    log and ws server

--- go.mod
+++ go.mod
@@ -4,15 +4,15 @@ go 1.16
 
 require (
 	github.com/CESSProject/cess-ffi v0.0.0-20220217052609-6c35c99d795c
-	github.com/HaoyuHu/gosimhash v0.0.0-20171130162857-733e771035c5
 	github.com/centrifuge/go-substrate-rpc-client/v3 v3.0.2
 	github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.0
-	github.com/corona10/goimagehash v1.0.3
-	github.com/dchest/siphash v1.2.2 // indirect
+	github.com/deckarep/golang-set v1.7.1
 	github.com/filecoin-project/go-fil-commcid v0.1.0 // indirect
 	github.com/filecoin-project/go-state-types v0.1.1
 	github.com/filecoin-project/specs-actors v0.9.13
 	github.com/filecoin-project/specs-actors/v5 v5.0.4 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/gorilla/websocket v1.4.2
 	github.com/ipfs/go-cid v0.1.0
 	github.com/ipfs/go-ipfs-chunker v0.0.5
 	github.com/klauspost/reedsolomon v1.9.14
@@ -20,13 +20,11 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.0
-	github.com/yanyiwu/gojieba v1.1.2 // indirect
 	go.uber.org/zap v1.19.1
-	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
+	google.golang.org/protobuf v1.27.1
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
 
 replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
 
 replace github.com/CESSProject/cess-ffi => ./internal/ffi
-
--- tools/utl.go
+++ tools/utl.go
@@ -7,7 +7,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
-	"math/big"
 	"math/rand"
 	"net"
 	"os"
@@ -17,6 +16,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unsafe"
 
 	"github.com/pkg/errors"
 )
@@ -195,46 +195,10 @@ func WriteStringtoFile(content, fileName string) error {
 	return nil
 }
 
-var base58 = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
-
-//Base58 encode
-func Base58Encoding(str string) string {
-	strByte := []byte(str)
-	strTen := big.NewInt(0).SetBytes(strByte)
-	var modSlice []byte
-	for strTen.Cmp(big.NewInt(0)) > 0 {
-		mod := big.NewInt(0)
-		strTen58 := big.NewInt(58)
-		strTen.DivMod(strTen, strTen58, mod)
-		modSlice = append(modSlice, base58[mod.Int64()])
-	}
-
-	for _, elem := range strByte {
-		if elem != 0 {
-			break
-		} else if elem == 0 {
-			modSlice = append(modSlice, byte('1'))
-		}
-	}
-	ReverseModSlice := ReverseByteArr(modSlice)
-	return string(ReverseModSlice)
+func B2S(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
 
-func ReverseByteArr(bytes []byte) []byte {
-	for i := 0; i < len(bytes)/2; i++ {
-		bytes[i], bytes[len(bytes)-1-i] = bytes[len(bytes)-1-i], bytes[i]
-	}
-	return bytes
-}
-
-//Base58 Decode
-func Base58Decoding(str string) string {
-	strByte := []byte(str)
-	ret := big.NewInt(0)
-	for _, byteElem := range strByte {
-		index := bytes.IndexByte(base58, byteElem)
-		ret.Mul(ret, big.NewInt(58))
-		ret.Add(ret, big.NewInt(int64(index)))
-	}
-	return string(ret.Bytes())
+func S2b(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&s))
 }
