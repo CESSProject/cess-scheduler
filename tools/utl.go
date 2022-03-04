5daff353a71f004403fa3b7a328b6154f1830256
Ted zhang <ted@cess.one>
2022-03-04 17:23:36 +0800

    feat:aes、randomkey、cut file

--- tools/utl.go
+++ tools/utl.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"math/big"
 	"math/rand"
 	"net"
 	"os"
@@ -16,7 +17,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-	"unsafe"
 
 	"github.com/pkg/errors"
 )
@@ -195,10 +195,60 @@ func WriteStringtoFile(content, fileName string) error {
 	return nil
 }
 
-func B2S(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+//  ----------------------- Base58 -----------------------
+var base58 = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+
+//Base58 encode
+func Base58Encoding(str string) string {
+	strByte := []byte(str)
+	strTen := big.NewInt(0).SetBytes(strByte)
+	var modSlice []byte
+	for strTen.Cmp(big.NewInt(0)) > 0 {
+		mod := big.NewInt(0)
+		strTen58 := big.NewInt(58)
+		strTen.DivMod(strTen, strTen58, mod)
+		modSlice = append(modSlice, base58[mod.Int64()])
+	}
+
+	for _, elem := range strByte {
+		if elem != 0 {
+			break
+		} else if elem == 0 {
+			modSlice = append(modSlice, byte('1'))
+		}
+	}
+	ReverseModSlice := ReverseByteArr(modSlice)
+	return string(ReverseModSlice)
 }
 
-func S2b(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&s))
+func ReverseByteArr(bytes []byte) []byte {
+	for i := 0; i < len(bytes)/2; i++ {
+		bytes[i], bytes[len(bytes)-1-i] = bytes[len(bytes)-1-i], bytes[i]
+	}
+	return bytes
+}
+
+//Base58 Decode
+func Base58Decoding(str string) string {
+	strByte := []byte(str)
+	ret := big.NewInt(0)
+	for _, byteElem := range strByte {
+		index := bytes.IndexByte(base58, byteElem)
+		ret.Mul(ret, big.NewInt(58))
+		ret.Add(ret, big.NewInt(int64(index)))
+	}
+	return string(ret.Bytes())
+}
+
+//  ----------------------- Random key -----------------------
+const baseStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()[]{}+-*/_=."
+
+func GetRandomkey(length uint8) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano() + rand.Int63()))
+	bytes := make([]byte, length)
+	l := len(baseStr)
+	for i := uint8(0); i < length; i++ {
+		bytes[i] = baseStr[r.Intn(l)]
+	}
+	return string(bytes)
 }
